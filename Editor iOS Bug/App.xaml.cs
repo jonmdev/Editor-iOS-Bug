6592fdaf01216ea96c216abaafc278c17214eda5
mjondev <43948450+jonmdev@users.noreply.github.com>
2023-10-06 21:47:21 -0400

    Update App.xaml.cs

--- Editor iOS Bug/App.xaml.cs
+++ Editor iOS Bug/App.xaml.cs
@@ -22,7 +22,9 @@
             border.Content = editor;
 
             Label testLabel = new Label();
-            testLabel.Text = "Editor iOS Bug Demonstration:\n- Editor does not autoscroll downward as you fill the Editor with text (new lines past max height go down off screen)\n- Editor does not scroll with click and drag once past maxHeight in size.\n- There is a frame lag after resizing Editor and before resizing the parent so the Editor goes 'out of bounds' of its parent momentarily each time it grows vertically.";
+            string bugText = "Editor iOS Bug (.NET 7):\n- Editor does not autoscroll downward as you fill the Editor with text (new lines past max height go down off screen)\n- Editor does not scroll with click and drag once past maxHeight in size.\n- There is a frame lag after resizing Editor and before resizing the parent so the Editor goes 'out of bounds' of its parent momentarily each time it grows vertically.";
+            bugText += "\n\nEditor iOS Bug (.NET 8):\n- Still has a frame lag as you type new lines.\n- Scrolling by touch and drag is not constrained, so you can slide it up all the way out of view.\n- The new line auto-scrolling is glitchy - if you keep pressing enter on touch screen keyboard it will keep moving the white text field higher and higher until it goes out of view.\n- Could have option to let pulling up down past bounds and then elastically return to rest position, but should also have option to not allow scrolling by finger past the typical edge.";
+            testLabel.Text = bugText;
             vert.Children.Add(testLabel);
 
         }
